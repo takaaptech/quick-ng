1208f20526895b52460baed2f2c96396c9d4c2d0
dualface <dualface@gmail.com>
2015-06-24 16:09:12 +0800

    cocos luacompile use luajit

--- tools/cocos2d-console/plugins/plugin_luacompile/__init__.py
+++ tools/cocos2d-console/plugins/plugin_luacompile/__init__.py
@@ -22,73 +22,73 @@ import shutil
 
 import cocos
 
-############################################################ 
+############################################################
 #http://www.coolcode.org/archives/?article-307.html
-############################################################ 
+############################################################
 
-import struct 
+import struct
 
-_DELTA = 0x9E3779B9  
+_DELTA = 0x9E3779B9
 
-def _long2str(v, w):  
-    n = (len(v) - 1) << 2  
-    if w:  
-        m = v[-1]  
-        if (m < n - 3) or (m > n): return ''  
-        n = m  
-    s = struct.pack('<%iL' % len(v), *v)  
-    return s[0:n] if w else s  
-  
-def _str2long(s, w):  
-    n = len(s)  
-    m = (4 - (n & 3) & 3) + n  
-    s = s.ljust(m, "\0")  
-    v = list(struct.unpack('<%iL' % (m >> 2), s))  
-    if w: v.append(n)  
-    return v  
-  
-def encrypt(str, key):  
-    if str == '': return str  
-    v = _str2long(str, True)  
-    k = _str2long(key.ljust(16, "\0"), False)  
-    n = len(v) - 1  
-    z = v[n]  
-    y = v[0]  
-    sum = 0  
-    q = 6 + 52 // (n + 1)  
-    while q > 0:  
-        sum = (sum + _DELTA) & 0xffffffff  
-        e = sum >> 2 & 3  
-        for p in xrange(n):  
-            y = v[p + 1]  
-            v[p] = (v[p] + ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z))) & 0xffffffff  
-            z = v[p]  
-        y = v[0]  
-        v[n] = (v[n] + ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[n & 3 ^ e] ^ z))) & 0xffffffff  
-        z = v[n]  
-        q -= 1  
-    return _long2str(v, False)  
-  
-def decrypt(str, key):  
-    if str == '': return str  
-    v = _str2long(str, False)  
-    k = _str2long(key.ljust(16, "\0"), False)  
-    n = len(v) - 1  
-    z = v[n]  
-    y = v[0]  
-    q = 6 + 52 // (n + 1)  
-    sum = (q * _DELTA) & 0xffffffff  
-    while (sum != 0):  
-        e = sum >> 2 & 3  
-        for p in xrange(n, 0, -1):  
-            z = v[p - 1]  
-            v[p] = (v[p] - ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z))) & 0xffffffff  
-            y = v[p]  
-        z = v[n]  
-        v[0] = (v[0] - ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[0 & 3 ^ e] ^ z))) & 0xffffffff  
-        y = v[0]  
-        sum = (sum - _DELTA) & 0xffffffff  
-    return _long2str(v, True)  
+def _long2str(v, w):
+    n = (len(v) - 1) << 2
+    if w:
+        m = v[-1]
+        if (m < n - 3) or (m > n): return ''
+        n = m
+    s = struct.pack('<%iL' % len(v), *v)
+    return s[0:n] if w else s
+
+def _str2long(s, w):
+    n = len(s)
+    m = (4 - (n & 3) & 3) + n
+    s = s.ljust(m, "\0")
+    v = list(struct.unpack('<%iL' % (m >> 2), s))
+    if w: v.append(n)
+    return v
+
+def encrypt(str, key):
+    if str == '': return str
+    v = _str2long(str, True)
+    k = _str2long(key.ljust(16, "\0"), False)
+    n = len(v) - 1
+    z = v[n]
+    y = v[0]
+    sum = 0
+    q = 6 + 52 // (n + 1)
+    while q > 0:
+        sum = (sum + _DELTA) & 0xffffffff
+        e = sum >> 2 & 3
+        for p in xrange(n):
+            y = v[p + 1]
+            v[p] = (v[p] + ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z))) & 0xffffffff
+            z = v[p]
+        y = v[0]
+        v[n] = (v[n] + ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[n & 3 ^ e] ^ z))) & 0xffffffff
+        z = v[n]
+        q -= 1
+    return _long2str(v, False)
+
+def decrypt(str, key):
+    if str == '': return str
+    v = _str2long(str, False)
+    k = _str2long(key.ljust(16, "\0"), False)
+    n = len(v) - 1
+    z = v[n]
+    y = v[0]
+    q = 6 + 52 // (n + 1)
+    sum = (q * _DELTA) & 0xffffffff
+    while (sum != 0):
+        e = sum >> 2 & 3
+        for p in xrange(n, 0, -1):
+            z = v[p - 1]
+            v[p] = (v[p] - ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z))) & 0xffffffff
+            y = v[p]
+        z = v[n]
+        v[0] = (v[0] - ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[0 & 3 ^ e] ^ z))) & 0xffffffff
+        y = v[0]
+        sum = (sum - _DELTA) & 0xffffffff
+    return _long2str(v, True)
 
 
 
@@ -155,7 +155,7 @@ class CCPluginLuaCompile(cocos.CCPlugin):
         """
         # create folder for generated file
         luac_filepath = ""
-        # Unknow to remove 'c' 
+        # Unknow to remove 'c'
         relative_path = self.get_relative_path(luafile)+"c"
         luac_filepath = os.path.join(self._dst_dir, relative_path)
 
@@ -177,9 +177,9 @@ class CCPluginLuaCompile(cocos.CCPlugin):
         if cocos.os_is_win32():
             ret = os.path.join(self._workingdir, "bin", "luajit.exe")
         elif cocos.os_is_mac():
-            ret = os.path.join(self._workingdir, "bin", "lua", "luajit-mac")
+            ret = os.path.join(self._workingdir, "bin", "luajit-mac")
         elif cocos.os_is_linux():
-            ret = os.path.join(self._workingdir, "bin", "lua", "luajit-linux")
+            ret = os.path.join(self._workingdir, "bin", "luajit-linux")
 
         return ret
 
@@ -226,7 +226,7 @@ class CCPluginLuaCompile(cocos.CCPlugin):
                 if self._disable_compile:
                     shutil.copy(lua_file, dst_lua_file)
                 else:
-                    self.compile_lua(lua_file, dst_lua_file)                   
+                    self.compile_lua(lua_file, dst_lua_file)
 
                 if self._isEncrypt == True:
                     bytesFile = open(dst_lua_file, "rb+")
@@ -313,11 +313,3 @@ class CCPluginLuaCompile(cocos.CCPlugin):
             workingdir = os.path.realpath(os.path.dirname(__file__))
 
         self.init(options, workingdir)
-
-
-
-
-
-
-
-
